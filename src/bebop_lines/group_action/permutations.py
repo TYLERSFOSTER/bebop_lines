b2e7ba641b178be05648d05c24c99df7a000c3ee
TYLERSFOSTER <tylerisnotavailable@gmail.com>
2025-05-09 08:25:17 -0400

    light cleanup

--- src/bebop_lines/group_action/permutations.py
+++ src/bebop_lines/group_action/permutations.py
@@ -1,5 +1,5 @@
 """
-Class for 
+Permutation groups on N letters
 """
 from __future__ import annotations
 
@@ -12,10 +12,17 @@ import bebop_lines.group_action as ga
 
 
 class Permutations():
+  """
+  Class for dynamically collecting full sets of permutations on n letters,
+  for increasing n.
+  """
   def __init__(self):
     self.permutation_dict = {1:[[0]]}
 
   def fillout_to(self, n : int) -> None:
+    """
+    Fill out permutation_dict attribute up to n
+    """
     if n in self.permutation_dict:
       return
     else:
@@ -34,19 +41,30 @@ class Permutations():
         self.permutation_dict[element_count+1] = new_permutations
       return
 
-  def __getitem__(self, element_count):
+  def __len__(self) -> int:
+    return self.completed_upto()
+
+  def __getitem__(self, element_count : int) -> dict[int, list[list[int]]]:
     if element_count > self.completed_upto():
       self.fillout_to(element_count)
 
     return self.permutation_dict[element_count]
 
-  def completed_upto(self):
+  def completed_upto(self) -> int:
+    """
+    Return the largest number of letters on which all permutations
+    have been stored as attributes
+    """
     return max(self.permutation_dict)
 
 
 
 class PermutationGroup():
-  def __init__(self, number_of_elements, permutations : ga.Permutations | None=None):
+  """
+  Class representing the group of permutations on the set {0, 1, 2, ...., N-1},
+  where N is the number_of_elements
+  """
+  def __init__(self, number_of_elements : int, permutations : ga.Permutations | None=None):
     if isinstance(permutations, type(None)):
       permutations = Permutations()
 
@@ -55,17 +73,20 @@ class PermutationGroup():
     self.underlying_set = permutations[number_of_elements]
     self.identity = [n for n in range(number_of_elements)]
 
-  def __len__(self):
+  def __len__(self) -> int:
     length = len(self.underlying_set)
 
     return length
 
-  def __getitem__(self, idx):
+  def __getitem__(self, idx : int) -> list[int]:
     element = self.underlying_set[idx]
 
     return element
 
-  def compose(self, elem_0, elem_1):
+  def compose(self, elem_0 : list[int], elem_1 : list[int]) -> list[int]:
+    """
+    Compose two permutations in the permutation group
+    """
     assert isinstance(elem_0, list)
     assert isinstance(elem_1, list)
     assert len(elem_0) == self.number_of_elements
@@ -75,7 +96,13 @@ class PermutationGroup():
 
     return composite
 
-  def invert(self, element):
+  def invert(self, element : list[int]) -> list[int]:
+    """
+    Invert a permutation in the permutation group
+    """
+    assert isinstance(element, list)
+    assert len(element) == self.number_of_elements
+
     inverse_element = []
 
     for idx in range(self.number_of_elements):
